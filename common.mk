a9f96ee7bc7c35b6967ce4c4e35c78dc4ec8ab4c
Koichi Sasada <ko1@atdot.net>
2020-07-09 03:33:08 +0900

    gc.c depends on vm_debug.h

--- common.mk
+++ common.mk
@@ -5820,6 +5820,7 @@ gc.$(OBJEXT): {$(VPATH)}transient_heap.h
 gc.$(OBJEXT): {$(VPATH)}util.h
 gc.$(OBJEXT): {$(VPATH)}vm_callinfo.h
 gc.$(OBJEXT): {$(VPATH)}vm_core.h
+gc.$(OBJEXT): {$(VPATH)}vm_debug.h
 gc.$(OBJEXT): {$(VPATH)}vm_opts.h
 gc.$(OBJEXT): {$(VPATH)}vm_sync.h
 ractor.$(OBJEXT): {$(VPATH)}config.h
