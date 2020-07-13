10f1e54eb9b2cbe72d9c1e28b18044fb6c935c61
Koichi Sasada <ko1@atdot.net>
2020-07-13 16:31:54 +0900

    WIP: fix atfork

--- thread.c
+++ thread.c
@@ -4705,10 +4705,11 @@ static void
 rb_thread_atfork_internal(rb_thread_t *th, void (*atfork)(rb_thread_t *, const rb_thread_t *))
 {
     rb_thread_t *i = 0;
-    rb_ractor_t *r;
     rb_vm_t *vm = th->vm;
-    vm->ractor.main_ractor = th->ractor;
+    rb_ractor_t *r = th->ractor;
+    vm->ractor.main_ractor = r;
     vm->ractor.main_thread = th;
+    r->status_ = ractor_created;
 
     gvl_atfork(rb_ractor_gvl(th->ractor));
     ubf_list_atfork();
@@ -4719,7 +4720,10 @@ rb_thread_atfork_internal(rb_thread_t *th, void (*atfork)(rb_thread_t *, const r
             atfork(i, th);
         }
     }
+    list_head_init(&vm->ractor.set);
+    vm->ractor.cnt = 0;
     rb_ractor_living_threads_init(th->ractor);
+
     rb_ractor_living_threads_insert(th->ractor, th);
 
     /* may be held by MJIT threads in parent */
@@ -4732,6 +4736,8 @@ rb_thread_atfork_internal(rb_thread_t *th, void (*atfork)(rb_thread_t *, const r
     vm->fork_gen++;
     rb_ractor_sleeper_threads_clear(th->ractor);
     rb_clear_coverages();
+
+    VM_ASSERT(vm->ractor.cnt == 1);
 }
 
 static void
