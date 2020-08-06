1b3160af8d2925afa8be63d2a781e0a6b04c8f91
Koichi Sasada <ko1@atdot.net>
2020-08-06 22:33:15 +0900

    WIP: export rb_ractor_std*

--- ractor_pub.h
+++ ractor_pub.h
@@ -1,11 +1,4 @@
 
-VALUE rb_ractor_stdin(void);
-VALUE rb_ractor_stdout(void);
-VALUE rb_ractor_stderr(void);
-void rb_ractor_stdin_set(VALUE);
-void rb_ractor_stdout_set(VALUE);
-void rb_ractor_stderr_set(VALUE);
-
 int rb_ractor_main_p(void);
 
 bool rb_ractor_shareable_p_continue(VALUE obj);
@@ -27,3 +20,14 @@ rb_ractor_shareable_p(VALUE obj)
         return rb_ractor_shareable_p_continue(obj);
     }
 }
+
+RUBY_SYMBOL_EXPORT_BEGIN
+
+VALUE rb_ractor_stdin(void);
+VALUE rb_ractor_stdout(void);
+VALUE rb_ractor_stderr(void);
+void rb_ractor_stdin_set(VALUE);
+void rb_ractor_stdout_set(VALUE);
+void rb_ractor_stderr_set(VALUE);
+
+RUBY_SYMBOL_EXPORT_END
